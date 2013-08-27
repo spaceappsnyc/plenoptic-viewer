378d43a6d5ccee66c7d70bdb6ebc1c74cee0861f
James <james@thedigitalself.com>
2013-08-27 13:58:47 -0400

    added mouse rotation

--- app/application/configuration-constants.js
+++ app/application/configuration-constants.js
@@ -5,5 +5,6 @@
 Application.Constants.constant('configuration', {
     ITEMS_URL : 'menu/items.json',
     DATA_URL : 'data/',
-    DEPTH_MAP_SUFFIX : '-stk_depth.txt'
+    DEPTH_MAP_SUFFIX : '-stk_depth.txt',
+    RENDERER_CAMERA_Z : 15
 });
